ed145ce9c50940f7fe69f91ae7ba6ec7eb72da3a
FluSett <flusett@gmail.com>
2022-10-02 15:45:15 +0300

    Authentication States

---
+++ lib/src/feature/authentication/logic/authentication_state.freezed.dart
@@ -0,0 +1,595 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'authentication_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$AuthenticationState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initital,
+    required TResult Function() progress,
+    required TResult Function(String message) error,
+    required TResult Function() success,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_InitialState value) initital,
+    required TResult Function(_AuthenticationProgressState value) progress,
+    required TResult Function(AuthenticationErrorState value) error,
+    required TResult Function(_AuthenticationSuccessState value) success,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AuthenticationStateCopyWith<$Res> {
+  factory $AuthenticationStateCopyWith(
+          AuthenticationState value, $Res Function(AuthenticationState) then) =
+      _$AuthenticationStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$AuthenticationStateCopyWithImpl<$Res>
+    implements $AuthenticationStateCopyWith<$Res> {
+  _$AuthenticationStateCopyWithImpl(this._value, this._then);
+
+  final AuthenticationState _value;
+  // ignore: unused_field
+  final $Res Function(AuthenticationState) _then;
+}
+
+/// @nodoc
+abstract class _$$_InitialStateCopyWith<$Res> {
+  factory _$$_InitialStateCopyWith(
+          _$_InitialState value, $Res Function(_$_InitialState) then) =
+      __$$_InitialStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_InitialStateCopyWithImpl<$Res>
+    extends _$AuthenticationStateCopyWithImpl<$Res>
+    implements _$$_InitialStateCopyWith<$Res> {
+  __$$_InitialStateCopyWithImpl(
+      _$_InitialState _value, $Res Function(_$_InitialState) _then)
+      : super(_value, (v) => _then(v as _$_InitialState));
+
+  @override
+  _$_InitialState get _value => super._value as _$_InitialState;
+}
+
+/// @nodoc
+
+class _$_InitialState implements _InitialState {
+  const _$_InitialState();
+
+  @override
+  String toString() {
+    return 'AuthenticationState.initital()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_InitialState);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initital,
+    required TResult Function() progress,
+    required TResult Function(String message) error,
+    required TResult Function() success,
+  }) {
+    return initital();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+  }) {
+    return initital?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+    required TResult orElse(),
+  }) {
+    if (initital != null) {
+      return initital();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_InitialState value) initital,
+    required TResult Function(_AuthenticationProgressState value) progress,
+    required TResult Function(AuthenticationErrorState value) error,
+    required TResult Function(_AuthenticationSuccessState value) success,
+  }) {
+    return initital(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+  }) {
+    return initital?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+    required TResult orElse(),
+  }) {
+    if (initital != null) {
+      return initital(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _InitialState implements AuthenticationState {
+  const factory _InitialState() = _$_InitialState;
+}
+
+/// @nodoc
+abstract class _$$_AuthenticationProgressStateCopyWith<$Res> {
+  factory _$$_AuthenticationProgressStateCopyWith(
+          _$_AuthenticationProgressState value,
+          $Res Function(_$_AuthenticationProgressState) then) =
+      __$$_AuthenticationProgressStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_AuthenticationProgressStateCopyWithImpl<$Res>
+    extends _$AuthenticationStateCopyWithImpl<$Res>
+    implements _$$_AuthenticationProgressStateCopyWith<$Res> {
+  __$$_AuthenticationProgressStateCopyWithImpl(
+      _$_AuthenticationProgressState _value,
+      $Res Function(_$_AuthenticationProgressState) _then)
+      : super(_value, (v) => _then(v as _$_AuthenticationProgressState));
+
+  @override
+  _$_AuthenticationProgressState get _value =>
+      super._value as _$_AuthenticationProgressState;
+}
+
+/// @nodoc
+
+class _$_AuthenticationProgressState implements _AuthenticationProgressState {
+  const _$_AuthenticationProgressState();
+
+  @override
+  String toString() {
+    return 'AuthenticationState.progress()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_AuthenticationProgressState);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initital,
+    required TResult Function() progress,
+    required TResult Function(String message) error,
+    required TResult Function() success,
+  }) {
+    return progress();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+  }) {
+    return progress?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+    required TResult orElse(),
+  }) {
+    if (progress != null) {
+      return progress();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_InitialState value) initital,
+    required TResult Function(_AuthenticationProgressState value) progress,
+    required TResult Function(AuthenticationErrorState value) error,
+    required TResult Function(_AuthenticationSuccessState value) success,
+  }) {
+    return progress(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+  }) {
+    return progress?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+    required TResult orElse(),
+  }) {
+    if (progress != null) {
+      return progress(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _AuthenticationProgressState implements AuthenticationState {
+  const factory _AuthenticationProgressState() = _$_AuthenticationProgressState;
+}
+
+/// @nodoc
+abstract class _$$AuthenticationErrorStateCopyWith<$Res> {
+  factory _$$AuthenticationErrorStateCopyWith(_$AuthenticationErrorState value,
+          $Res Function(_$AuthenticationErrorState) then) =
+      __$$AuthenticationErrorStateCopyWithImpl<$Res>;
+  $Res call({String message});
+}
+
+/// @nodoc
+class __$$AuthenticationErrorStateCopyWithImpl<$Res>
+    extends _$AuthenticationStateCopyWithImpl<$Res>
+    implements _$$AuthenticationErrorStateCopyWith<$Res> {
+  __$$AuthenticationErrorStateCopyWithImpl(_$AuthenticationErrorState _value,
+      $Res Function(_$AuthenticationErrorState) _then)
+      : super(_value, (v) => _then(v as _$AuthenticationErrorState));
+
+  @override
+  _$AuthenticationErrorState get _value =>
+      super._value as _$AuthenticationErrorState;
+
+  @override
+  $Res call({
+    Object? message = freezed,
+  }) {
+    return _then(_$AuthenticationErrorState(
+      message == freezed
+          ? _value.message
+          : message // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$AuthenticationErrorState implements AuthenticationErrorState {
+  const _$AuthenticationErrorState(this.message);
+
+  @override
+  final String message;
+
+  @override
+  String toString() {
+    return 'AuthenticationState.error(message: $message)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$AuthenticationErrorState &&
+            const DeepCollectionEquality().equals(other.message, message));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(message));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$AuthenticationErrorStateCopyWith<_$AuthenticationErrorState>
+      get copyWith =>
+          __$$AuthenticationErrorStateCopyWithImpl<_$AuthenticationErrorState>(
+              this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initital,
+    required TResult Function() progress,
+    required TResult Function(String message) error,
+    required TResult Function() success,
+  }) {
+    return error(message);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+  }) {
+    return error?.call(message);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error(message);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_InitialState value) initital,
+    required TResult Function(_AuthenticationProgressState value) progress,
+    required TResult Function(AuthenticationErrorState value) error,
+    required TResult Function(_AuthenticationSuccessState value) success,
+  }) {
+    return error(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+  }) {
+    return error?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class AuthenticationErrorState implements AuthenticationState {
+  const factory AuthenticationErrorState(final String message) =
+      _$AuthenticationErrorState;
+
+  String get message;
+  @JsonKey(ignore: true)
+  _$$AuthenticationErrorStateCopyWith<_$AuthenticationErrorState>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class _$$_AuthenticationSuccessStateCopyWith<$Res> {
+  factory _$$_AuthenticationSuccessStateCopyWith(
+          _$_AuthenticationSuccessState value,
+          $Res Function(_$_AuthenticationSuccessState) then) =
+      __$$_AuthenticationSuccessStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_AuthenticationSuccessStateCopyWithImpl<$Res>
+    extends _$AuthenticationStateCopyWithImpl<$Res>
+    implements _$$_AuthenticationSuccessStateCopyWith<$Res> {
+  __$$_AuthenticationSuccessStateCopyWithImpl(
+      _$_AuthenticationSuccessState _value,
+      $Res Function(_$_AuthenticationSuccessState) _then)
+      : super(_value, (v) => _then(v as _$_AuthenticationSuccessState));
+
+  @override
+  _$_AuthenticationSuccessState get _value =>
+      super._value as _$_AuthenticationSuccessState;
+}
+
+/// @nodoc
+
+class _$_AuthenticationSuccessState implements _AuthenticationSuccessState {
+  const _$_AuthenticationSuccessState();
+
+  @override
+  String toString() {
+    return 'AuthenticationState.success()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_AuthenticationSuccessState);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initital,
+    required TResult Function() progress,
+    required TResult Function(String message) error,
+    required TResult Function() success,
+  }) {
+    return success();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+  }) {
+    return success?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initital,
+    TResult Function()? progress,
+    TResult Function(String message)? error,
+    TResult Function()? success,
+    required TResult orElse(),
+  }) {
+    if (success != null) {
+      return success();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_InitialState value) initital,
+    required TResult Function(_AuthenticationProgressState value) progress,
+    required TResult Function(AuthenticationErrorState value) error,
+    required TResult Function(_AuthenticationSuccessState value) success,
+  }) {
+    return success(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+  }) {
+    return success?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_InitialState value)? initital,
+    TResult Function(_AuthenticationProgressState value)? progress,
+    TResult Function(AuthenticationErrorState value)? error,
+    TResult Function(_AuthenticationSuccessState value)? success,
+    required TResult orElse(),
+  }) {
+    if (success != null) {
+      return success(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _AuthenticationSuccessState implements AuthenticationState {
+  const factory _AuthenticationSuccessState() = _$_AuthenticationSuccessState;
+}
