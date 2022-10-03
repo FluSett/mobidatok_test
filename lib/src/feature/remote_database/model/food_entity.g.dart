cdb6a9e96d06552421302ca32143ef16cb79d875
FluSett <flusett@gmail.com>
2022-10-03 04:12:05 +0300

    Food Entity

---
+++ lib/src/feature/remote_database/model/food_entity.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'food_entity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_FoodEntity _$$_FoodEntityFromJson(Map<String, dynamic> json) =>
+    _$_FoodEntity(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      price: json['price'] as String,
+    );
+
+Map<String, dynamic> _$$_FoodEntityToJson(_$_FoodEntity instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'price': instance.price,
+    };
